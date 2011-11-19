a238ba0ef416f78f0d15f5142dce45582e1b87e3
Domen Kožar <domen@dev.si>
2011-11-19 17:28:41 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,3 @@
+Place titanpad_backup.sh in ~/titanpad/ and configure crontab as following::
+	
+	0 0 * * * ~/titanpad/titanpad_backup.sh <username> <password>
---
+++ titanpad_backup.sh
@@ -0,0 +1,30 @@
+DOMAIN="ie"
+COOKIES=~/.titanpad_cookies
+LOCATION=~/titanpad/titanpad_backup_pads_$(date "+%Y-%m-%d_%k-%M").zip
+
+touch $COOKIES
+mkdir -p ~/titanpad
+wget    --no-check-certificate \
+	--keep-session-cookies \
+	--quiet \
+	-O /dev/null \
+	--save-cookies ~/.titanpad_cookies \
+	"https://$DOMAIN.titanpad.com/"
+wget    --no-check-certificate \
+	--load-cookies ~/.titanpad_cookies \
+	--quiet \
+	-O /dev/null \
+	--post-data "email=$1&password=$2" \
+	"https://$DOMAIN.titanpad.com/ep/account/sign-in" 
+wget    --load-cookies ~/.titanpad_cookies \
+	--no-check-certificate \
+	--quiet \
+	-O "$LOCATION" \
+	https://$DOMAIN.titanpad.com/ep/padlist/all-pads.zip
+rm $COOKIES
+
+# verify zip file
+zip -T "$LOCATION"
+
+# delete older files than a month
+find ~/titanpad/titanpad_backup_pads* -mtime +30 -exec rm {} \;
