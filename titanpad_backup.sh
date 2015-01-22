784823627e67cd383c1fedfc3f3d1ee66c1a051d
Konstantin Baierer <konstantin.baierer@bib.uni-mannheim.de>
2015-01-22 10:27:37 +0100

    Made code more flexible (use getopts, make domain an option)

--- titanpad_backup.sh
+++ titanpad_backup.sh
@@ -1,30 +1,87 @@
-DOMAIN="ie"
-COOKIES=~/.titanpad_cookies
+DOMAIN=
+USER=
+PASSWORD=
+PURGE_OLD_FILES=
+
+COOKIE=~/titanpad/.cookie
 LOCATION=~/titanpad/titanpad_backup_pads_$(date "+%Y-%m-%d").zip
 
-touch $COOKIES
-mkdir -p ~/titanpad
-wget    --no-check-certificate \
-	--keep-session-cookies \
-	--quiet \
-	-O /dev/null \
-	--save-cookies ~/.titanpad_cookies \
-	"https://$DOMAIN.titanpad.com/"
-wget    --no-check-certificate \
-	--load-cookies ~/.titanpad_cookies \
-	--quiet \
-	-O /dev/null \
-	--post-data "email=$1&password=$2" \
-	"https://$DOMAIN.titanpad.com/ep/account/sign-in" 
-wget    --load-cookies ~/.titanpad_cookies \
-	--no-check-certificate \
-	--quiet \
-	-O "$LOCATION" \
-	https://$DOMAIN.titanpad.com/ep/padlist/all-pads.zip
-rm $COOKIES
+usage() { echo "Usage: $0 -d <subdomain> -u <user> -p <password>" 1>&2; exit 1; }
+
+# make sure backup directory exists
+ensure_paths() {
+    mkdir -p ~/titanpad
+    touch $COOKIE
+}
+
+# login and set cookie
+login() {
+    wget --no-check-certificate \
+         --keep-session-cookies \
+         -O /dev/null \
+         --save-cookies $COOKIE \
+        "https://$DOMAIN.titanpad.com/"
+    wget --no-check-certificate \
+         --load-cookies $COOKIE \
+         -O /dev/null \
+         --post-data "email=$USER&password=$PASSWORD" \
+         "https://$DOMAIN.titanpad.com/ep/account/sign-in" 
+}
+
+# remove cookie
+logout() {
+    rm $COOKIE
+}
+
+# download the newest pad
+download_newest_pad() {
+    wget --load-cookies $COOKIE \
+         --no-check-certificate \
+         --quiet \
+         -O "$LOCATION" \
+         "https://$DOMAIN.titanpad.com/ep/padlist/all-pads.zip"
+}
 
 # verify zip file
-unzip -t "$LOCATION"
+verify_download() { 
+    unzip -t "$LOCATION"
+}
 
 # delete older files than a month
-find ~/titanpad/titanpad_backup_pads* -mtime +30 -exec rm {} \;
+delete_old_files() {
+    if [ "$PURGE_OLD_FILES" == 1 ];then
+        find ~/titanpad/titanpad_backup_pads* -mtime +30 -exec rm {} \;
+    fi
+}
+
+
+while getopts ":d:u:p:x" o; do
+    case "${o}" in
+        d)
+            DOMAIN=${OPTARG}
+            ;;
+        u)
+            USER=${OPTARG}
+            ;;
+        p)
+            PASSWORD=${OPTARG}
+            ;;
+        x)
+            PURGE_OLD_FILES=1
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+if [ -z "$DOMAIN" ] || [ -z "$USER" ] || [ -z "$PASSWORD" ]; then
+    usage
+fi
+
+ensure_paths
+login
+download_newest_pad
+verify_download
+logout
+delete_old_files
